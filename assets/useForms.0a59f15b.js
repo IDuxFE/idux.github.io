6c59bf8e949a9a31253a68715110e4498a4ca470
IDuxTeam <idux.feteam@gmail.com>
2022-03-01 12:23:45 +0000

    chore: release 1.0.0-beta.0

--- assets/useForms.0a59f15b.js
+++
@@ -1,6 +0,0 @@
-import{h as u,j as s,k as c,l}from"./index.dbdf6cd1.js";/**
- * @license
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
- */function p(r,o,t){const n=i(r);return new u(n,o,t)}function F(r,o,t){const n=r.map(a=>e(a));return new c(n,o,t)}function f(r,o,t){return new s(r,o,t)}function i(r){const o={};return Object.keys(r).forEach(t=>{o[t]=e(r[t])}),o}function e(r){if(l(r))return r;{const[o,t,n]=r;return new s(o,t,n)}}export{F as a,f as b,p as u};
