7c6dde08742390e468c886b8f23a173624c32932
danranvm <danranvm@gmail.com>
2022-11-09 11:02:25 +0800

    chore: release 1.1.0

---
+++ assets/index.596cb727.js
@@ -0,0 +1,6 @@
+import{T as a}from"./Textarea.d24ab93b.js";import"./index.c56724f3.js";/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const e=a;export{e as I};
---
+++ assets/index.625846dd.js
@@ -0,0 +1,6 @@
+import{I as t}from"./Input.f12f5d4a.js";import"./index.c56724f3.js";/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const I=t;export{I};
