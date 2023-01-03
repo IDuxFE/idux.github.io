d54014f98beb63626cd4d9a634b0b901756db495
IDuxTeam <idux.feteam@gmail.com>
2023-01-03 07:46:02 +0000

    chore: release 1.3.2

---
+++ assets/index-14e717c9.js
@@ -0,0 +1,6 @@
+import{u as S,E as z}from"./index-b887bcb8.js";import{i as A,u as B}from"./useInput-db9b21c2.js";import{v as R,b as F,x as G,y as M}from"./vendor-4521613a.js";const N=R({name:"IxInput",props:A,setup(e,{slots:n,expose:a}){const s=S("input"),{elementRef:t,accessor:r,mergedSize:l,mergedStatus:u,clearable:i,clearIcon:d,clearVisible:c,isFocused:f,focus:p,blur:m,handleInput:I,handleCompositionStart:b,handleCompositionEnd:v,handleClear:x,syncValue:C}=B(e,s);a({focus:p,blur:m});const o=F();return G(()=>{t.value=o.value.getInputElement(),C()}),()=>{const{addonAfter:g,addonBefore:h,borderless:y,prefix:V,suffix:E}=e;return M(z,{ref:o,addonAfter:g,addonBefore:h,borderless:y,clearable:i.value,clearIcon:d.value,clearVisible:c.value,disabled:r.disabled,focused:f.value,prefix:V,size:l.value,status:u.value,suffix:E,onClear:x,readonly:e.readonly,onInput:I,onCompositionstart:b,onCompositionend:v},n)}}});/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const q=N;export{q as I};
