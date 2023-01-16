7ea6014d3d8f3537438afe00de25854413ae0b0c
IDuxTeam <idux.feteam@gmail.com>
2023-01-16 09:02:27 +0000

    chore: release 1.4.0

---
+++ assets/tree-e660bbd9.js
@@ -0,0 +1,6 @@
+import{k as p,O}from"./vendor-bc3fa58e.js";/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */function I(u,o,e,r="pre"){const n=(t,s)=>{for(let f=0;f<t.length;f++){const i=t[f];r==="pre"&&e(i,s),i[o]&&n(i[o],[i,...s]),r==="post"&&e(i,s)}};n(u,[])}function k(u,o,e,r="or"){const n=(t,s)=>{var i;const f=[];for(let a=0;a<t.length;a++){let m;const c=t[a];c[o]&&(m=n(c[o],[c,...s]));let l=e(c,s);const g=m&&m.length>0||!((i=c[o])!=null&&i.length)&&l;l=r==="and"?g&&l:g||l,l&&f.push({...c,[o]:c[o]&&m})}return f};return n(u,[])}function T(u,o,e,r){const n=u.map(t=>({...t}));for(const t of o){const s=n.find(f=>{const i=r(t),a=r(f);return!p(i)&&!p(a)&&i===a});if(!s){n.push({...t});continue}for(const f of[...Object.getOwnPropertyNames(t),...Object.getOwnPropertySymbols(t)])f!==e&&(s[f]=t[f]);O(t[e])&&(s[e]=T(s[e]??[],t[e],e,r))}return n}function v(u,o,e,r=!1){const n=[];return I(u,o,t=>{if(!r||!t[o]||t[o].length<=0){const s=e?e(t):t;s&&n.push(s)}}),n}function x(u,o,e,r=!1){const n=[];return I(u,o,t=>{var s;(!r||!((s=t[o])!=null&&s.length))&&n.push(e(t))}),n}export{v as a,k as f,x as g,T as m,I as t};
