01194e52ea0d34c8a462d3d954e6be8faa6c4b6a
danranvm <danranvm@gmail.com>
2023-07-06 18:53:16 +0800

    chore: release 1.8.0

--- assets/index-464e6847.js
+++
@@ -1,6 +0,0 @@
-import{L as S,ac as A}from"./index-a5e3e5c4.js";import{i as B,u as E}from"./useInput-49b42cb0.js";import{x as R,r as F,y as G,z as L}from"./vendor-8f7e6b71.js";const M=R({name:"IxInput",props:B,setup(e,{slots:n,expose:a}){const s=S("input"),{elementRef:t,accessor:r,mergedSize:l,mergedStatus:u,clearable:i,clearIcon:d,clearVisible:c,isFocused:f,focus:p,blur:m,handleInput:I,handleCompositionStart:b,handleCompositionEnd:v,handleClear:x,syncValue:C}=E(e,s);a({focus:p,blur:m});const o=F();return G(()=>{t.value=o.value.getInputElement(),C()}),()=>{const{addonAfter:g,addonBefore:h,borderless:y,prefix:V,suffix:z}=e;return L(A,{ref:o,addonAfter:g,addonBefore:h,borderless:y,clearable:i.value,clearIcon:d.value,clearVisible:c.value,disabled:r.disabled,focused:f.value,prefix:V,size:l.value,status:u.value,suffix:z,onClear:x,readonly:e.readonly,onInput:I,onCompositionstart:b,onCompositionend:v},n)}}});/**
- * @license
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
- */const k=M;export{k as I};
---
+++ assets/index-c0aa2ef8.js
@@ -0,0 +1,6 @@
+import{L as S,ac as A}from"./index-72d58f2a.js";import{i as B,u as E}from"./useInput-060cb25c.js";import{x as R,r as F,y as G,z as L}from"./vendor-64a846c1.js";const M=R({name:"IxInput",props:B,setup(e,{slots:n,expose:a}){const s=S("input"),{elementRef:t,accessor:r,mergedSize:l,mergedStatus:u,clearable:i,clearIcon:d,clearVisible:c,isFocused:f,focus:p,blur:m,handleInput:I,handleCompositionStart:b,handleCompositionEnd:v,handleClear:x,syncValue:C}=E(e,s);a({focus:p,blur:m});const o=F();return G(()=>{t.value=o.value.getInputElement(),C()}),()=>{const{addonAfter:g,addonBefore:h,borderless:y,prefix:V,suffix:z}=e;return L(A,{ref:o,addonAfter:g,addonBefore:h,borderless:y,clearable:i.value,clearIcon:d.value,clearVisible:c.value,disabled:r.disabled,focused:f.value,prefix:V,size:l.value,status:u.value,suffix:z,onClear:x,readonly:e.readonly,onInput:I,onCompositionstart:b,onCompositionend:v},n)}}});/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const k=M;export{k as I};
