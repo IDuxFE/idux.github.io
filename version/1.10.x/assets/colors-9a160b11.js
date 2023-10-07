04b3cb04aa4311884a7939822dd2d5f3977c8874
sallerli1 <weskerli@163.com>
2023-10-07 18:44:08 +0800

    chore: add 1.10.x version docs

---
+++ version/1.10.x/assets/colors-9a160b11.js
@@ -0,0 +1,6 @@
+/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const e=["red","orange","brown","yellow","canary","bud","green","turquoise","cyan","glacier","blue","indigo","purple","magenta"];function o(r){return e.includes(r)}const n=["primary","info","pending","success","warning","error"];function s(r){return n.includes(r)}export{s as a,o as i};
---
+++ version/1.10.x/assets/index-4c408d21.js
@@ -0,0 +1,11 @@
+import{L as d,O as $}from"./index-30b90d3b.js";import{x as h,c,A as x,z as p}from"./vendor-5c3e2579.js";/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const g={dashed:{type:Boolean,default:void 0},label:String,labelPlacement:String,plain:{type:Boolean,default:void 0},size:String,vertical:{type:Boolean,default:void 0}},C=h({name:"IxDivider",props:g,setup(o,{slots:n}){const v=d("common"),s=c(()=>`${v.prefixCls}-divider`),a=d("divider"),m=c(()=>{const{dashed:t=a.dashed,label:l,labelPlacement:u=a.labelPlacement,plain:f=a.plain,size:b=a.size,vertical:r}=o,i=!!l||!!n.default,e=s.value;return x({[`${e}`]:!0,[`${e}-${b}`]:!0,[`${e}-dashed`]:t,[`${e}-horizontal`]:!r,[`${e}-vertical`]:r,[`${e}-plain`]:i&&f,[`${e}-with-label`]:i,[`${e}-with-label-${u}`]:i})});return()=>{const t=s.value,l=$(n.default,o.label);return p("div",{class:m.value},[l&&p("span",{class:`${t}-label`},[l])])}}});/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const S=C;export{S as I};
