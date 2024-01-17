1974d8462404ecdd6fd62a33bd9be6687989389a
IDuxTeam <idux.feteam@gmail.com>
2024-01-17 11:43:33 +0000

    chore: release 2.0.0-beta.6

---
+++ assets/index-887372da.js
@@ -0,0 +1,26 @@
+import{L as C,M as $,N as R,a6 as z,A,ao as E,K as j,k as D,m as K}from"./index-8abf4cd4.js";import{l as G,x as H,c as h,n as L,p as g,d as W,B as M,ad as U}from"./vendor-1c4f0380.js";/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const w=Symbol("tag");/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const V={bordered:{type:Boolean,default:void 0},filled:{type:Boolean,default:void 0},icon:String,number:Number,shape:String,status:{type:String,default:"normal"}},q={activeKeys:{type:Array,default:()=>[]},bordered:{type:Boolean,default:void 0},clickable:{type:Boolean,default:!1},closable:{type:Boolean,default:!1},closeIcon:{type:String,default:"close"},filled:{type:Boolean,default:void 0},dataSource:Array,gap:[Number,String],wrap:{type:Boolean,default:void 0},shape:String,"onUpdate:activeKeys":[Function,Array],onClick:[Function,Array],onClose:[Function,Array]};/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */function N(t,o){const{lineWidth:u,borderRadiusSm:m,tagCompColorAlpha:l,colorTextInfo:v,colorSuccessBg:B,colorInfoBg:i,colorWarningBg:a,colorRiskBg:d,colorErrorBg:p,colorFatalBg:y}=t,{getGreyColors:I}=o,T=I();return{bgColorNormal:C(v,l),bgColorFilled:T.base,borderWidth:u,borderRadius:m,minWidthRect:40,minWidthRound:48,minWidthNumeric:64,successBgColor:C(B,l),infoBgColor:C(i,l),warningBgColor:C(a,l),riskBgColor:C(d,l),errorBgColor:C(p,l),fatalBgColor:C(y,l)}}/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const J=(t,o,u)=>N(t,u),F=G({name:"IxTag",props:V,setup(t,{slots:o}){const u=$("common"),{globalHashId:m,hashId:l,registerToken:v}=R("tag");v(J);const B=$("tag"),i=z(),{props:a,mergedClosedKeys:d,handleTagClick:p,handleTagClose:y}=H(w,{}),I=h(()=>`${u.prefixCls}-tag`),T=n=>{a!=null&&a.clickable&&(p==null||p(i,n))},e=n=>{a!=null&&a.closable&&(n.stopPropagation(),y==null||y(i))},r=h(()=>{const{shape:n=(a==null?void 0:a.shape)||B.shape,bordered:c=a==null?void 0:a.bordered,filled:s=a==null?void 0:a.filled,number:x,status:k}=t,f=I.value,b=!W(x);return L({[m.value]:!!m.value,[l.value]:!!l.value,[`${f}`]:!0,[`${f}-${k}`]:!0,[`${f}-${n}`]:!b&&n,[`${f}-bordered`]:c,[`${f}-filled`]:s,[`${f}-numeric`]:b})});return()=>{var S;if(d!=null&&d.value.includes(i))return null;const n=I.value,{icon:c,number:s}=t,{closable:x,closeIcon:k}=a||{},f=o.icon?o.icon():c&&g(A,{name:c},null);let b=o.suffix?o.suffix():x&&g(A,{name:k},null);return x&&(b=g("span",{class:`${n}-close-icon`,onClick:e},[b])),g("span",{class:r.value,onClick:T},[O(n,s),f,g("span",{class:`${n}-content`},[(S=o.default)==null?void 0:S.call(o)]),b])}}});function O(t,o){return W(o)?null:g("span",{class:`${t}-numeric-prefix`},[o>9?"9+":o])}const Q=G({name:"IxTagGroup",props:q,setup(t,{slots:o}){const u=$("common"),m=$("tagGroup"),l=h(()=>`${u.prefixCls}-tag-group`),v=h(()=>{var e;return(e=t.gap)!=null?e:m.gap}),B=h(()=>{var e;return(e=t.wrap)!=null?e:m.wrap}),[i,a]=E(t,"activeKeys",()=>[]),[d,p]=j([]);M(w,{props:t,mergedActiveKeys:i,mergedClosedKeys:d,handleTagClick:(e,r)=>{K(t.onClick,e,r);const n=[...i.value],c=n.findIndex(s=>s===e);c===-1?n.push(e):n.splice(c,1),a(n)},handleTagClose:async e=>{if(await K(t.onClose,e)!==!1){const n=i.value,c=n.findIndex(s=>s===e);if(c!==-1){const s=[...n];s.splice(c,1),a(s)}p([...d.value,e])}}});const T=h(()=>{const e=l.value,r=v.value;return{[e]:!0,[`${e}-clickable`]:t.clickable,[`${e}-compact`]:!r||r==="0"}});return()=>g(D,{class:T.value,size:v.value,wrap:B.value},{default:()=>{var e,r;return[((e=t.dataSource)==null?void 0:e.map((n,c)=>{const{key:s=c,label:x,...k}=n;return g(F,U(k,{key:s}),{default:()=>[x],suffix:o.closeIcon})}))||((r=o.default)==null?void 0:r.call(o))]}})}});/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const Z=F,_=Q;export{Z as I,_ as a};
---
+++ assets/index-eafa38c6.js
@@ -0,0 +1,31 @@
+import{bg as h,Q as S,M as P,N as B,A as k,O as D}from"./index-8abf4cd4.js";import{l as y,x as z,c as a,n as $,p as o,h as M,B as E,au as _}from"./vendor-1c4f0380.js";/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const w=["red","orange","brown","yellow","canary","bud","green","turquoise","cyan","glacier","blue","indigo","purple","magenta"];function L(e){return w.includes(e)}const W=["primary","info","pending","success","warning","error"];function K(e){return W.includes(e)}/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const T=Symbol("timelineToken"),x=Symbol("IDUX_TIMELINE_ITEM_KEY");/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const O={pending:{type:[String,Boolean],default:!1},pendingDot:String,reverse:{type:Boolean,default:!1},placement:{type:String,default:"end"},both:{type:Boolean,default:!0}},V={color:{type:String,default:"primary"},dot:String,label:String,placement:String},N=y({name:"IxTimelineItem",props:V,[x]:!0,setup(e,{slots:t}){const{props:l,mergedPrefixCls:r}=z(T),{vnode:i}=M(),v=a(()=>{const{placement:n}=l,{placement:s}=e,u=i.index;let I;return n==="alternate"?I=s||(u%2?"start":"end"):I=n,I}),c=a(()=>L(e.color)||K(e.color)),d=a(()=>{const{color:n}=e;return c.value?{}:{color:n,"border-color":n}}),m=a(()=>c.value?{}:{"background-color":e.color}),g=a(()=>{const n=`${r.value}-item`,s=v.value;return $({[`${n}`]:!0,[`${n}-${s}`]:!0})}),f=a(()=>h(t,"dot")||!!e.dot),p=a(()=>{const n=`${r.value}-item-head`,{color:s}=e;return $({[`${n}-dot`]:!0,[`${n}-dot-custom`]:f.value,[`${n}-dot-${s}`]:c.value})});return()=>{const{label:n,dot:s}=e,u=`${r.value}-item`,I=S(t.dot,s),C=S(t.label,n);return o("li",{class:g.value},[o("div",{class:`${u}-head`},[o("div",{class:`${u}-head-line`},null),o("div",{class:p.value,style:d.value},[I,!f.value&&o("div",{class:`${u}-head-dot-inner`,style:m.value},null)])]),o("div",{class:`${u}-content`},[t.default&&o("div",{class:`${u}-content-desc`},[t.default()]),C&&o("div",{class:`${u}-content-label`},[C])])])}}});/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */function b(e){const{fontSizeSm:t,marginSizeSm:l,marginSizeLg:r,colorSeparator:i}=e;return{fontSize:t,dotSize:12,dotBorderWidth:2,contentLabelMarginTop:l,lineWidth:2,lineBgColor:i,pendingItemContentMinWidth:40,contentMarginBottom:r}}/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const j=(e,t)=>b(e),q=y({name:"IxTimeline",props:O,setup(e,{slots:t}){const l=P("common"),{globalHashId:r,hashId:i,registerToken:v}=B("timeline");v(j);const c=a(()=>`${l.prefixCls}-timeline`),d=a(()=>e.pending!==!1||h(t,"pending")),m=a(()=>{const{placement:g,reverse:f,both:p}=e,n=c.value;return $({[r.value]:!!r.value,[i.value]:!!i.value,[`${n}`]:!0,[`${n}-${g}`]:!0,[`${n}-reverse`]:f,[`${n}-not-both`]:!p,[`${n}-pending`]:d.value})});return E(T,{props:e,mergedPrefixCls:c}),()=>{const g=G(t,e,d,c);return o("ul",{class:m.value},[g.value])}}});function A(e){return D(e,{key:x}).map((t,l)=>(t.index=l,t))}function G(e,t,l,r){return a(()=>{var f,p;const{pending:i,pendingDot:v,reverse:c}=t,d=((f=e==null?void 0:e.default)==null?void 0:f.call(e))||[];let m=null;if(l.value){const n=((p=e==null?void 0:e.pendingDot)==null?void 0:p.call(e))||v||o(k,{name:"loading"},null);m=o(N,{class:`${r.value}-item-pending`},{default:()=>[S(e.pending,_(i)?"":i)],dot:()=>n})}const g=c?[m,...d.reverse()]:[...d,m];return A(g.filter(Boolean))})}/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const U=q,X=N;export{X as I,U as a};
