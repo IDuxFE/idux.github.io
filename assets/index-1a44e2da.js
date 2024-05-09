d977b8ba7d441da8486f926fe5891f6f576f930b
IDuxTeam <idux.feteam@gmail.com>
2024-05-09 13:29:10 +0000

    chore: release 2.2.0

--- assets/index-1a44e2da.js
+++
@@ -1,6 +0,0 @@
-import{M as z,a9 as A}from"./index-c8d09f7e.js";import{i as B,u as E}from"./useInput-4bdd825e.js";import{l as M,r as R,m as q,q as F}from"./vendor-754a3ca4.js";const G=M({name:"IxInput",props:B,setup(e,{slots:a,expose:s}){const t=z("input"),{elementRef:r,accessor:o,mergedSize:l,mergedStatus:u,clearable:i,clearIcon:d,clearVisible:c,isFocused:f,focus:p,blur:m,handleInput:I,handleCompositionStart:b,handleCompositionEnd:v,handleClear:C,syncValue:x}=E(e,t);s({focus:p,blur:m});const n=R();return q(()=>{r.value=n.value.getInputElement(),x()}),()=>{const{addonAfter:g,addonBefore:h,borderless:V,prefix:y,suffix:S}=e;return F(A,{ref:n,addonAfter:g,addonBefore:h,borderless:V,clearable:i.value,clearIcon:d.value,clearVisible:c.value,disabled:o.disabled,focused:f.value,prefix:y,readonly:e.readonly,size:l.value,status:u.value,suffix:S,value:o.value,onClear:C,onInput:I,onCompositionstart:b,onCompositionend:v},a)}}});/**
- * @license
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
- */const k=G;export{k as I};
---
+++ assets/index-438c637a.js
@@ -0,0 +1,31 @@
+import{bh as b,a5 as S,M as P,N as B,A as D,am as k}from"./index-a29fd331.js";import{l as y,y as z,c as a,p as $,q as o,h as M,D as E,au as _}from"./vendor-4501f550.js";/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const w=["red","orange","brown","yellow","canary","bud","green","turquoise","cyan","glacier","blue","indigo","purple","magenta"];function L(e){return w.includes(e)}const W=["primary","info","pending","success","warning","error"];function q(e){return W.includes(e)}/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const T=Symbol("timelineToken"),x=Symbol("IDUX_TIMELINE_ITEM_KEY");/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const K={pending:{type:[String,Boolean],default:!1},pendingDot:String,reverse:{type:Boolean,default:!1},placement:{type:String,default:"end"},both:{type:Boolean,default:!0}},V={color:{type:String,default:"primary"},dot:String,label:String,placement:String},N=y({name:"IxTimelineItem",props:V,[x]:!0,setup(e,{slots:t}){const{props:l,mergedPrefixCls:r}=z(T),{vnode:i}=M(),v=a(()=>{const{placement:n}=l,{placement:s}=e,u=i.index;let I;return n==="alternate"?I=s||(u%2?"start":"end"):I=n,I}),c=a(()=>L(e.color)||q(e.color)),d=a(()=>{const{color:n}=e;return c.value?{}:{color:n,"border-color":n}}),m=a(()=>c.value?{}:{"background-color":e.color}),g=a(()=>{const n=`${r.value}-item`,s=v.value;return $({[`${n}`]:!0,[`${n}-${s}`]:!0})}),f=a(()=>b(t,"dot")||!!e.dot),p=a(()=>{const n=`${r.value}-item-head`,{color:s}=e;return $({[`${n}-dot`]:!0,[`${n}-dot-custom`]:f.value,[`${n}-dot-${s}`]:c.value})});return()=>{const{label:n,dot:s}=e,u=`${r.value}-item`,I=S(t.dot,s),h=S(t.label,n);return o("li",{class:g.value},[o("div",{class:`${u}-head`},[o("div",{class:`${u}-head-line`},null),o("div",{class:p.value,style:d.value},[I,!f.value&&o("div",{class:`${u}-head-dot-inner`,style:m.value},null)])]),o("div",{class:`${u}-content`},[t.default&&o("div",{class:`${u}-content-desc`},[t.default()]),h&&o("div",{class:`${u}-content-label`},[h])])])}}});/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */function C(e){const{fontSizeSm:t,marginSizeSm:l,marginSizeLg:r,colorSeparator:i}=e;return{fontSize:t,dotSize:12,dotBorderWidth:2,contentLabelMarginTop:l,lineWidth:2,lineBgColor:i,pendingItemContentMinWidth:40,contentMarginBottom:r}}/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const j=(e,t)=>C(e),A=y({name:"IxTimeline",props:K,setup(e,{slots:t}){const l=P("common"),{globalHashId:r,hashId:i,registerToken:v}=B("timeline");v(j);const c=a(()=>`${l.prefixCls}-timeline`),d=a(()=>e.pending!==!1||b(t,"pending")),m=a(()=>{const{placement:g,reverse:f,both:p}=e,n=c.value;return $({[r.value]:!!r.value,[i.value]:!!i.value,[`${n}`]:!0,[`${n}-${g}`]:!0,[`${n}-reverse`]:f,[`${n}-not-both`]:!p,[`${n}-pending`]:d.value})});return E(T,{props:e,mergedPrefixCls:c}),()=>{const g=H(t,e,d,c);return o("ul",{class:m.value},[g.value])}}});function G(e){return k(e,{key:x}).map((t,l)=>(t.index=l,t))}function H(e,t,l,r){return a(()=>{var f,p;const{pending:i,pendingDot:v,reverse:c}=t,d=((f=e==null?void 0:e.default)==null?void 0:f.call(e))||[];let m=null;if(l.value){const n=((p=e==null?void 0:e.pendingDot)==null?void 0:p.call(e))||v||o(D,{name:"loading"},null);m=o(N,{class:`${r.value}-item-pending`},{default:()=>[S(e.pending,_(i)?"":i)],dot:()=>n})}const g=c?[m,...d.reverse()]:[...d,m];return G(g.filter(Boolean))})}/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const X=A,Y=N;export{Y as I,X as a};
