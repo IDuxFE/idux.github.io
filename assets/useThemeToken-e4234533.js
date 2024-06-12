49cb48b882c5100f8fc4878dab254ee9c0d59137
IDuxTeam <idux.feteam@gmail.com>
2024-06-12 16:27:00 +0000

    chore: release 2.2.3

--- assets/useThemeToken-e4234533.js
+++
@@ -1,6 +0,0 @@
-import{N as o}from"./index-9cb41eac.js";/**
- * @license
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
- */function r(e){return o(e)}export{r as u};
