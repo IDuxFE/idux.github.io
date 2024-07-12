be5387c7c0532a42143b4f41dad507728ad63b8f
IDuxTeam <idux.feteam@gmail.com>
2024-07-12 09:18:32 +0000

    chore: release 2.3.1

--- assets/index-02427ea9.js
+++
@@ -1,6 +0,0 @@
-import{T as z,ag as A}from"./index-4a5449a2.js";import{i as B,u as E}from"./useInput-e6d71fa6.js";import{m as R,r as F,n as G,v as M}from"./vendor-e200cecb.js";const N=R({name:"IxInput",props:B,setup(e,{slots:a,expose:s}){const t=z("input"),{elementRef:r,accessor:n,mergedSize:l,mergedStatus:u,clearable:i,clearIcon:d,clearVisible:c,isFocused:f,focus:p,blur:m,handleInput:I,handleCompositionStart:v,handleCompositionEnd:b,handleClear:C,syncValue:x}=E(e,t);s({focus:p,blur:m});const o=F();return G(()=>{r.value=o.value.getInputElement(),x()}),()=>{const{addonAfter:g,addonBefore:h,borderless:V,prefix:y,suffix:S}=e;return M(A,{ref:o,addonAfter:g,addonBefore:h,borderless:V,clearable:i.value,clearIcon:d.value,clearVisible:c.value,disabled:n.disabled,focused:f.value,prefix:y,readonly:e.readonly,size:l.value,status:u.value,suffix:S,value:n.value,onClear:C,onInput:I,onCompositionstart:v,onCompositionend:b},a)}}});/**
- * @license
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
- */const k=N;export{k as I};
---
+++ assets/index-4aa476ff.js
@@ -0,0 +1,16 @@
+import{e as y,T as $,U as S,aL as h,aC as L,aM as P,b as z}from"./index-974a2c75.js";import{m as C,z as j,c as b,v as a,F as T,E as F,q as G}from"./vendor-e200cecb.js";/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const p=Symbol("list");/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const O={header:String,footer:String,empty:{type:[String,Object],default:"default"},borderless:{type:Boolean,default:void 0},loading:{type:[Boolean,Object],default:!1},size:String,grid:Object},k={title:String,content:String,extra:String},B=C({name:"IxListItem",props:k,setup(n,{slots:e}){const{props:v,mergedPrefixCls:x}=j(p),i=b(()=>`${x.value}-item`),o=(r,t)=>{const c=r[t];if(typeof c=="number")return Math.floor(24/c)},I=r=>{const t=o(r,"column");return{xs:o(r,"xs")||t,sm:o(r,"sm")||t,md:o(r,"md")||t,ld:o(r,"lg")||t,xl:o(r,"xl")||t}};return()=>{var d,u,f,m,g;const r=v.grid&&I(v.grid),t=(u=n.title)!=null?u:(d=e.title)==null?void 0:d.call(e),c=(m=n.content)!=null?m:(f=e.default)==null?void 0:f.call(e),s=(g=e.extra)==null?void 0:g.call(e),l=a(T,null,[t&&a("div",{class:`${i.value}-title`},[t]),a("div",{class:`${i.value}-content`},[c]),s&&a("div",{class:`${i.value}-extra`},[s])]);return r?a(y,r,{default:()=>[l]}):a("div",{class:`${i.value}`},[l])}}}),E=C({name:"IxList",props:O,setup(n,{slots:e}){const v=$("common"),{globalHashId:x}=S(),i=b(()=>`${v.prefixCls}-list`),o=$("list");F(p,{props:n,mergedPrefixCls:i});const I=b(()=>typeof n.loading=="boolean"?{spinning:n.loading}:n.loading),r=b(()=>{var l,d;const t=i.value,c=(l=n.borderless)!=null?l:o.borderless,s=(d=n.size)!=null?d:o.size;return G({[x.value]:!x.value,[`${t}`]:!0,[`${t}-split`]:!0,[`${t}-${s}`]:!0,[`${t}-border`]:!c})});return()=>{var u,f,m,g;const t=(f=n.header)!=null?f:(u=e.header)==null?void 0:u.call(e),c=(g=n.footer)!=null?g:(m=e.footer)==null?void 0:m.call(e),s=e.default?e.default():[],l=h(s)?s:a(L,{empty:n.empty},e),d=()=>n.grid?a(z,{gutter:n.grid.gutter},{default:()=>[l]}):a("div",null,[l]);return a("div",{class:r.value},[t&&a("div",{class:`${i.value}-header`},[t]),a(P,I.value,{default:()=>[d()]}),c&&a("div",{class:`${i.value}-footer`},[c])])}}});/**
+ * @license
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/IDuxFE/idux/blob/main/LICENSE
+ */const V=B,q=E;export{q as I,V as a};
